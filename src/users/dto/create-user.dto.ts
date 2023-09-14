7c019700164be55f5ef74837cfe9e68eef194c7b
Elena Tatsenko <lena.harchenko24@gmail.com>
2023-09-14 15:30:20 +0300

    add validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,12 @@
+import {IsEmail, IsString, Length} from "class-validator";
+
 export class CreateUserDto {
+    
+    @IsString({message: 'Must be a string'})
+    @IsEmail({}, {message: "Incorrect email"})
     readonly email: string;
+    
+    @IsString({message: 'Must be a string'})
+    @Length(4, 16, {message: 'Not less than 4 and not more than 16'})
     readonly password: string;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,16 @@
-import { Controller, Post, Body, UseGuards, Get } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Get, UsePipes } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import {Roles} from "../auth/roles-auth.decorator";
 import {RolesGuard} from "../auth/roles.guard";
 import { AddRoleDto } from './dto/add-role.dto';
+import { ValidationPipe } from 'src/pipes/validation.pipes';
 
 @Controller('users')
 export class UsersController {
     constructor(private userService: UsersService) {
-
     }
-    @Post()
+     @Post()
     create(@Body() userDto: CreateUserDto) {
         return this.userService.createUser(userDto);
     }
